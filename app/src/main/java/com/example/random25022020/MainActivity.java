ffea18ed97e8521c42391035480d71a19e9260b2
luongthanhdat0707 <thanhdat070788@gmail.com>
2020-03-14 21:15:14 +0700

    làm tròn

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.random25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Math
+        // Lam tron
+        Double a= 1.2 ;
+//        long value = Math.round(a);
+//        Lam tron len
+//        double value = Math.ceil(a);
+//        Lam tron xuống
+//        double value2 = Math.floor(a);
+//        Log.d("BBB",String.valueOf(value2));
     }
 }
